033be16cbe8c8c7b32c4d5b9552f16c5cbe8b9ef
Thomas Jackson <jacksontj.89@gmail.com>
2017-10-14 10:37:11 -0700

    Remove unused variable

--- cmd/promxy/main.go
+++ cmd/promxy/main.go
@@ -3,13 +3,10 @@ package main
 import (
 	"github.com/jacksontj/promxy/promxy"
 	"github.com/jessevdk/go-flags"
-	"github.com/prometheus/prometheus/promql"
 	"github.com/sirupsen/logrus"
 )
 
 //http://localhost:8080/api/v1/query?query=scrape_duration_seconds%5B1m%5D&time=1507256489.103&_=1507256486365
-
-var engine promql.Engine
 
 var opts struct {
 	ConfigFile string `long:"config" description:"path to the config file" required:"true"`
