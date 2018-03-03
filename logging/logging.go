ef33571b3595f50c48c56ff6699dcb5e96bfdfa2
Thomas Jackson <jacksontj.89@gmail.com>
2018-03-03 13:17:59 -0800

    Commit logging (missing from previous commits)

---
+++ logging/logging.go
@@ -0,0 +1,79 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const ApacheFormatPattern = "%s - - [%s] \"%s %d %d\" %f\n"
+
+type ApacheLogRecord struct {
+	http.ResponseWriter
+
+	ip                    string
+	time                  time.Time
+	method, uri, protocol string
+	status                int
+	responseBytes         int64
+	elapsedTime           time.Duration
+}
+
+func (r *ApacheLogRecord) Log(out io.Writer) {
+	timeFormatted := r.time.Format("02/Jan/2006 03:04:05")
+	requestLine := fmt.Sprintf("%s %s %s", r.method, r.uri, r.protocol)
+	fmt.Fprintf(out, ApacheFormatPattern, r.ip, timeFormatted, requestLine, r.status, r.responseBytes,
+		r.elapsedTime.Seconds())
+}
+
+func (r *ApacheLogRecord) Write(p []byte) (int, error) {
+	written, err := r.ResponseWriter.Write(p)
+	r.responseBytes += int64(written)
+	return written, err
+}
+
+func (r *ApacheLogRecord) WriteHeader(status int) {
+	r.status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+type ApacheLoggingHandler struct {
+	handler http.Handler
+	out     io.Writer
+}
+
+func NewApacheLoggingHandler(handler http.Handler, out io.Writer) http.Handler {
+	return &ApacheLoggingHandler{
+		handler: handler,
+		out:     out,
+	}
+}
+
+func (h *ApacheLoggingHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	clientIP := r.RemoteAddr
+	if colon := strings.LastIndex(clientIP, ":"); colon != -1 {
+		clientIP = clientIP[:colon]
+	}
+
+	record := &ApacheLogRecord{
+		ResponseWriter: rw,
+		ip:             clientIP,
+		time:           time.Time{},
+		method:         r.Method,
+		uri:            r.RequestURI,
+		protocol:       r.Proto,
+		status:         http.StatusOK,
+		elapsedTime:    time.Duration(0),
+	}
+
+	startTime := time.Now()
+	h.handler.ServeHTTP(record, r)
+	finishTime := time.Now()
+
+	record.time = finishTime.UTC()
+	record.elapsedTime = finishTime.Sub(startTime)
+
+	record.Log(h.out)
+}
