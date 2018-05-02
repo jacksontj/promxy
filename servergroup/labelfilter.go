838243ed14489c6659061fd1d02a3f870a2fcb2a
Thomas Jackson <jacksontj.89@gmail.com>
2018-05-01 17:17:41 -0700

    Apply matchers to labels attached to the servergroup
    This way if you have labels "az" we don't send those down to the
    underlying prometheus hosts, we instead filter it in promxy

---
+++ servergroup/labelfilter.go
@@ -0,0 +1,49 @@
+package servergroup
+
+import (
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/prometheus/prometheus/promql"
+)
+
+type LabelFilterVisitor struct {
+	s           *ServerGroup
+	filterMatch bool
+}
+
+func (l *LabelFilterVisitor) Visit(node promql.Node, path []promql.Node) (w promql.Visitor) {
+	switch nodeTyped := node.(type) {
+	case *promql.VectorSelector:
+		for _, matcher := range nodeTyped.LabelMatchers {
+			if matcher.Name == model.MetricNameLabel && matcher.Type == labels.MatchEqual {
+				nodeTyped.Name = matcher.Value
+			}
+		}
+
+		filteredMatchers, ok := l.s.FilterMatchers(nodeTyped.LabelMatchers)
+		l.filterMatch = l.filterMatch && ok
+
+		if ok {
+			nodeTyped.LabelMatchers = filteredMatchers
+		} else {
+			return nil
+		}
+	case *promql.MatrixSelector:
+		for _, matcher := range nodeTyped.LabelMatchers {
+			if matcher.Name == model.MetricNameLabel && matcher.Type == labels.MatchEqual {
+				nodeTyped.Name = matcher.Value
+			}
+		}
+
+		filteredMatchers, ok := l.s.FilterMatchers(nodeTyped.LabelMatchers)
+		l.filterMatch = l.filterMatch && ok
+
+		if ok {
+			nodeTyped.LabelMatchers = filteredMatchers
+		} else {
+			return nil
+		}
+	}
+
+	return l
+}
