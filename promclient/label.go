370136b473b0fd2c83717c733b6940e40f0fdd76
Thomas Jackson <jacksontj.89@gmail.com>
2018-07-12 15:30:24 -0700

    Remove old unused methods

--- promclient/label.go
+++ promclient/label.go
@@ -1,8 +1,6 @@
 package promclient
 
 import (
-	"fmt"
-
 	"github.com/jacksontj/promxy/promhttputil"
 	"github.com/prometheus/common/model"
 )
@@ -13,31 +11,6 @@ type LabelResult struct {
 	Data      []model.LabelValue     `json:"data"`
 	ErrorType promhttputil.ErrorType `json:"errorType,omitempty"`
 	Error     string                 `json:"error,omitempty"`
-}
-
-func (l *LabelResult) Merge(o *LabelResult) error {
-	if l.Status == "" {
-		l.Status = o.Status
-		l.Data = o.Data
-		return nil
-	}
-	// TODO: need to know all the types and have logic -- pick the worst of the bunch
-	if l.Status != o.Status {
-		return fmt.Errorf("mismatch status")
-	}
-
-	labels := make(map[model.LabelValue]struct{})
-	for _, item := range l.Data {
-		labels[item] = struct{}{}
-	}
-
-	for _, item := range o.Data {
-		if _, ok := labels[item]; !ok {
-			l.Data = append(l.Data, item)
-			labels[item] = struct{}{}
-		}
-	}
-	return nil
 }
 
 func MergeLabelValues(a, b []model.LabelValue) []model.LabelValue {
--- promclient/series.go
+++ promclient/series.go
@@ -1,8 +1,6 @@
 package promclient
 
 import (
-	"fmt"
-
 	"github.com/jacksontj/promxy/promhttputil"
 	"github.com/prometheus/common/model"
 )
@@ -13,32 +11,4 @@ type SeriesResult struct {
 	Data      []model.LabelSet       `json:"data"`
 	ErrorType promhttputil.ErrorType `json:"errorType,omitempty"`
 	Error     string                 `json:"error,omitempty"`
-}
-
-func (s *SeriesResult) Merge(o *SeriesResult) error {
-	if s.Status == "" {
-		s.Status = o.Status
-		s.Data = o.Data
-		return nil
-	}
-	// TODO: need to know all the types and have logic -- pick the worst of the bunch
-	if s.Status != o.Status {
-		return fmt.Errorf("mismatch status")
-	}
-
-	sigs := make(map[model.Fingerprint]struct{})
-	for _, res := range s.Data {
-		sigs[res.Fingerprint()] = struct{}{}
-	}
-
-	// TODO; dedupe etc.
-	for _, result := range o.Data {
-		// calculate sig
-		sig := result.Fingerprint()
-		if _, ok := sigs[sig]; !ok {
-			s.Data = append(s.Data, result)
-			sigs[sig] = struct{}{}
-		}
-	}
-	return nil
 }
