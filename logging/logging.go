928cb99bf759d81cf6fb9c3925fdaef06b500400
Thomas Jackson <jacksontj.89@gmail.com>
2018-09-27 20:27:14 -0700

    Make the recover() handler always return a stack trace

--- logging/logging.go
+++ logging/logging.go
@@ -65,11 +65,8 @@ func NewApacheLoggingHandler(handler http.Handler, logHandlers ...LogRecordHandl
 func (h *ApacheLoggingHandler) runHandler(rw http.ResponseWriter, r *http.Request) (err error) {
 	defer func() {
 		if rec := recover(); rec != nil {
-			var ok bool
-			err, ok = rec.(error)
-			if !ok {
-				err = errors.Wrap(fmt.Errorf(string(debug.Stack())), "Error running handler")
-			}
+			// Just return a stack trace always
+			err = errors.Wrap(fmt.Errorf(string(debug.Stack())), "Error running handler")
 		}
 	}()
 	h.handler.ServeHTTP(rw, r)
