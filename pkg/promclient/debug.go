b0a9e3081ebcb9a1a208892c12b23607414d772e
Thomas Jackson <jacksontj.89@gmail.com>
2019-07-02 10:30:26 -0700

    Add timings to debug logging of servergroups
    Now we add took to debug, and reserve value/warnings/error for trace
    level logging

--- pkg/promclient/debug.go
+++ pkg/promclient/debug.go
@@ -19,112 +19,126 @@ type DebugAPI struct {
 
 // LabelValues performs a query for the values of the given label.
 func (d *DebugAPI) LabelValues(ctx context.Context, label string) (model.LabelValues, api.Warnings, error) {
-	logrus.WithFields(logrus.Fields{
+	fields := logrus.Fields{
 		"api":   "LabelValues",
 		"label": label,
-	}).Debug(d.PrefixMessage)
+	}
+	logrus.WithFields(fields).Debug(d.PrefixMessage)
 
+	s := time.Now()
 	v, w, err := d.API.LabelValues(ctx, label)
+	fields["took"] = time.Now().Sub(s)
 
-	logrus.WithFields(logrus.Fields{
-		"api":      "LabelValues",
-		"label":    label,
-		"value":    v,
-		"warnings": w,
-		"error":    err,
-	}).Trace(d.PrefixMessage)
+	if logrus.GetLevel() > logrus.DebugLevel {
+		fields["value"] = v
+		fields["warnings"] = w
+		fields["error"] = err
+		logrus.WithFields(fields).Trace(d.PrefixMessage)
+	} else {
+		logrus.WithFields(fields).Debug(d.PrefixMessage)
+	}
 
 	return v, w, err
 }
 
 // Query performs a query for the given time.
 func (d *DebugAPI) Query(ctx context.Context, query string, ts time.Time) (model.Value, api.Warnings, error) {
-	logrus.WithFields(logrus.Fields{
+	fields := logrus.Fields{
 		"api":   "Query",
 		"query": query,
 		"ts":    ts,
-	}).Debug(d.PrefixMessage)
+	}
+	logrus.WithFields(fields).Debug(d.PrefixMessage)
 
+	s := time.Now()
 	v, w, err := d.API.Query(ctx, query, ts)
+	fields["took"] = time.Now().Sub(s)
 
-	logrus.WithFields(logrus.Fields{
-		"api":      "Query",
-		"query":    query,
-		"ts":       ts,
-		"value":    v,
-		"warnings": w,
-		"error":    err,
-	}).Trace(d.PrefixMessage)
+	if logrus.GetLevel() > logrus.DebugLevel {
+		fields["value"] = v
+		fields["warnings"] = w
+		fields["error"] = err
+		logrus.WithFields(fields).Trace(d.PrefixMessage)
+	} else {
+		logrus.WithFields(fields).Debug(d.PrefixMessage)
+	}
 
 	return v, w, err
 }
 
 // QueryRange performs a query for the given range.
 func (d *DebugAPI) QueryRange(ctx context.Context, query string, r v1.Range) (model.Value, api.Warnings, error) {
-	logrus.WithFields(logrus.Fields{
+	fields := logrus.Fields{
 		"api":   "QueryRange",
 		"query": query,
 		"r":     r,
-	}).Debug(d.PrefixMessage)
+	}
+	logrus.WithFields(fields).Debug(d.PrefixMessage)
 
+	s := time.Now()
 	v, w, err := d.API.QueryRange(ctx, query, r)
+	fields["took"] = time.Now().Sub(s)
 
-	logrus.WithFields(logrus.Fields{
-		"api":      "QueryRange",
-		"query":    query,
-		"r":        r,
-		"value":    v,
-		"warnings": w,
-		"error":    err,
-	}).Trace(d.PrefixMessage)
+	if logrus.GetLevel() > logrus.DebugLevel {
+		fields["value"] = v
+		fields["warnings"] = w
+		fields["error"] = err
+		logrus.WithFields(fields).Trace(d.PrefixMessage)
+	} else {
+		logrus.WithFields(fields).Debug(d.PrefixMessage)
+	}
 
 	return v, w, err
 }
 
 // Series finds series by label matchers.
 func (d *DebugAPI) Series(ctx context.Context, matches []string, startTime time.Time, endTime time.Time) ([]model.LabelSet, api.Warnings, error) {
-	logrus.WithFields(logrus.Fields{
+	fields := logrus.Fields{
 		"api":       "Series",
 		"matches":   matches,
 		"startTime": startTime,
 		"endTime":   endTime,
-	}).Debug(d.PrefixMessage)
+	}
+	logrus.WithFields(fields).Debug(d.PrefixMessage)
 
+	s := time.Now()
 	v, w, err := d.API.Series(ctx, matches, startTime, endTime)
+	fields["took"] = time.Now().Sub(s)
 
-	logrus.WithFields(logrus.Fields{
-		"api":       "Series",
-		"matches":   matches,
-		"startTime": startTime,
-		"endTime":   endTime,
-		"value":     v,
-		"warnings":  w,
-		"error":     err,
-	}).Trace(d.PrefixMessage)
-
+	if logrus.GetLevel() > logrus.DebugLevel {
+		fields["value"] = v
+		fields["warnings"] = w
+		fields["error"] = err
+		logrus.WithFields(fields).Trace(d.PrefixMessage)
+	} else {
+		logrus.WithFields(fields).Debug(d.PrefixMessage)
+	}
 	return v, w, err
 }
 
 // GetValue loads the raw data for a given set of matchers in the time range
 func (d *DebugAPI) GetValue(ctx context.Context, start, end time.Time, matchers []*labels.Matcher) (model.Value, api.Warnings, error) {
-	logrus.WithFields(logrus.Fields{
+	fields := logrus.Fields{
 		"api":      "GetValue",
 		"start":    start,
 		"end":      end,
 		"matchers": matchers,
-	}).Debug(d.PrefixMessage)
+	}
 
+	logrus.WithFields(fields).Debug(d.PrefixMessage)
+
+	s := time.Now()
 	v, w, err := d.API.GetValue(ctx, start, end, matchers)
+	fields["took"] = time.Now().Sub(s)
 
-	logrus.WithFields(logrus.Fields{
-		"api":      "GetValue",
-		"start":    start,
-		"end":      end,
-		"matchers": matchers,
-		"value":    v,
-		"warnings": w,
-		"error":    err,
-	}).Trace(d.PrefixMessage)
+	if logrus.GetLevel() > logrus.DebugLevel {
+		fields["value"] = v
+		fields["warnings"] = w
+		fields["error"] = err
+		logrus.WithFields(fields).Trace(d.PrefixMessage)
+	} else {
+		logrus.WithFields(fields).Debug(d.PrefixMessage)
+	}
 
 	return v, w, err
 }
