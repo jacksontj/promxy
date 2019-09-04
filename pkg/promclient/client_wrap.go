7bd1cebd4a2b7d404ad1d9d3fb359decd53761d1
Thomas Jackson <jacksontj.89@gmail.com>
2019-09-04 08:05:44 -0700

    Add QueryParams (query_params) option to add params to downstream queries
    This enables users to configure query params to send to a given downstream ServerGroup. The initial case here is to solve #202 to allow the user to specify `nocache=1`
    
    Fixes #202

---
+++ pkg/promclient/client_wrap.go
@@ -0,0 +1,31 @@
+package promclient
+
+import (
+	"net/url"
+
+	"github.com/prometheus/client_golang/api"
+)
+
+// NewClientArgsWrap returns a client that will add the given args
+func NewClientArgsWrap(api api.Client, args map[string]string) *ClientArgsWrap {
+	return &ClientArgsWrap{api, args}
+}
+
+// ClientArgsWrap wraps the prom API client to add query params to any given urls
+type ClientArgsWrap struct {
+	api.Client
+
+	args map[string]string
+}
+
+func (c *ClientArgsWrap) URL(ep string, args map[string]string) *url.URL {
+	u := c.Client.URL(ep, args)
+
+	q := u.Query()
+	for k, v := range c.args {
+		q.Set(k, v)
+	}
+	u.RawQuery = q.Encode()
+
+	return u
+}
