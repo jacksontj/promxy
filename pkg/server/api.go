8f776cb405940504e3077f0aa239d5f278375932
Kira Boyle <kboyle@pivotal.io>
2021-06-23 10:53:20 -0700

    added ability to use define a tlsConfig [wip]
    Signed-off-by: Devon Warshaw <warshawd@vmware.com>

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -4,13 +4,14 @@ import (
 	"github.com/jacksontj/promxy/pkg/logging"
 	"github.com/julienschmidt/httprouter"
 	"github.com/prometheus/common/log"
+	"github.com/prometheus/exporter-toolkit/web"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 	"time"
 )
 
-func Placeholder(bindAddr string, logFormat string, webReadTimeout time.Duration, accessLogOut io.Writer, router *httprouter.Router) *http.Server{
+func Placeholder(bindAddr string, logFormat string, webReadTimeout time.Duration, accessLogOut io.Writer, router *httprouter.Router, tlsStruct *web.TLSStruct) (*http.Server, error) {
 	var handler http.Handler
 	if accessLogOut == nil {
 		handler = router
@@ -23,24 +24,31 @@ func Placeholder(bindAddr string, logFormat string, webReadTimeout time.Duration
 		}
 	}
 
-	//if opts.WebConfigFile == "" {
-	srv := &http.Server{
-		Addr:        bindAddr,
-		Handler:     handler,
-		TLSConfig:   nil,
-		ReadTimeout: webReadTimeout,
+	var srv *http.Server
+	if tlsStruct == nil {
+		srv = &http.Server{
+			Addr:        bindAddr,
+			Handler:     handler,
+			TLSConfig:   nil,
+			ReadTimeout: webReadTimeout,
+		}
+
+		go func() {
+			logrus.Infof("promxy starting")
+			if err := srv.ListenAndServe(); err != nil {
+				if err == http.ErrServerClosed {
+					return
+				}
+				log.Errorf("Error listening: %v", err)
+			}
+		}()
 	}
 
-	go func() {
-		logrus.Infof("promxy starting")
-		if err := srv.ListenAndServe(); err != nil {
-			if err == http.ErrServerClosed {
-				return
-			}
-			log.Errorf("Error listening: %v", err)
-		}
-	}()
-	//}
+	_, err := web.ConfigToTLSConfig(tlsStruct)
+	if err != nil {
+		return nil, err
+	}
+
 	//TODO return err
-	return srv
+	return srv, nil
 }
