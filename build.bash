0543dc241bc8f5b705ba29d9b87618c9c2c57451
Thomas Jackson <jacksontj.89@gmail.com>
2023-03-10 14:41:37 -0800

    Add promxy version metric
    This also replaces how we are storing the version to use the prometheus
    common format (makes metrics and printing match)
    
    Fixes #522

--- build.bash
+++ build.bash
@@ -2,6 +2,14 @@
 
 VERSION=`git describe --tags --dirty`
 
+ldflags_array=(
+    "-X github.com/prometheus/common/version.Version=$VERSION"
+    "-X github.com/prometheus/common/version.Revision=`git show -s --format=%H`"
+    "-X github.com/prometheus/common/version.Branch=`git rev-parse --abbrev-ref HEAD`"
+    "-X github.com/prometheus/common/version.BuildUser=$USER@$HOSTNAME"
+    "-X github.com/prometheus/common/version.BuildDate=`date +"%Y%m%d-%T"`"
+)
+
 package=$1
 destination=$2
 if [[ -z "$package" ]]; then
@@ -25,7 +33,7 @@ do
 
     env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 GO111MODULE=on \
         go build -mod=vendor -tags netgo,builtinassets -x \
-                -ldflags="-X main.Version=$VERSION" \
+                -ldflags="${ldflags_array[*]}" \
                 -o $destination/$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
