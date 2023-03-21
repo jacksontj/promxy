eca751a48fd16a6845546465298ac05aeca41f66
Thomas Jackson <jacksontj.89@gmail.com>
2023-03-21 14:43:27 -0700

    Appease go-lint
    Related to https://github.com/golang/lint/issues/366

--- go_mod_tidy_hack.go
+++ go_mod_tidy_hack.go
@@ -1,5 +1,7 @@
 package promxy
 
-// import autorest directly to avoid ambiguous import
-// check https://github.com/Azure/go-autorest/issues/414
-import _ "github.com/Azure/go-autorest/autorest"
+import (
+	// import autorest directly to avoid ambiguous import
+	// check https://github.com/Azure/go-autorest/issues/414
+	_ "github.com/Azure/go-autorest/autorest"
+)
