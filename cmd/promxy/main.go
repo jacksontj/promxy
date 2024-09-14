25786e60c68a5ab26a9a2ede2e8c8e920068fef2
Thomas Jackson <thomas@conduit.xyz>
2024-09-13 20:41:14 -0700

    Fix redirect loop with no prefix on debug endpoints
    Fixes #680

--- cmd/promxy/main.go
+++ cmd/promxy/main.go
@@ -448,7 +448,7 @@ func main() {
 	r.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Have our fallback rules
 		if strings.HasPrefix(r.URL.Path, path.Join(webOptions.RoutePrefix, "/debug")) {
-			http.StripPrefix(webOptions.RoutePrefix, http.DefaultServeMux).ServeHTTP(w, r)
+			http.StripPrefix(strings.Trim(webOptions.RoutePrefix, "/"), http.DefaultServeMux).ServeHTTP(w, r)
 		} else if r.URL.Path == path.Join(webOptions.RoutePrefix, "/-/ready") {
 			if stopping {
 				w.WriteHeader(http.StatusServiceUnavailable)
